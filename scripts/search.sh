c4e5e709c8df594c934139a3eedb1eefc9fc96c0
Mara Kim <hacker.root@gmail.com>
2013-04-30 15:40:58 -0500

    added search.sh

---
+++ scripts/search.sh
@@ -0,0 +1,31 @@
+# search
+# Search directories
+#
+# Copyright (C) 2013 Mara Kim, Kris McGary
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see http://www.gnu.org/licenses/.
+
+
+### USAGE ###
+# Source this file in your shell's .*rc file
+
+# search using find
+function search {
+if [ $1 ]
+then
+    find $PWD -name "$1"
+else
+    find $PWD
+fi
+}

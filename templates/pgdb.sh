ad9b4768c443f311d313e7f6f421257193a3f665
Mara Kim <hacker.root@gmail.com>
2014-03-14 16:10:59 -0500

    update to use eval for compatability

--- templates/pgdb.sh
+++ templates/pgdb.sh
@@ -1,5 +1,5 @@
 # pgdb
-# Template functions for managing a connection to a PostgreSQL database
+# Template function for managing a connection to a PostgreSQL database
 #
 # Copyright (C) 2013 Mara Kim
 #
@@ -20,182 +20,97 @@
 ### USAGE ###
 
 # Source this file in your shell's .*rc file
-# Then use template-mydb and template-mydbbackup
-# to create your functions
+#
+# Accepts the following arguments:
+# database: the PostgreSQL database
+# user: the PostgreSQL user
+# funcname: optional; the name of the generated function, default [database]
+# 
+# Declares the following function:
+# [FUNCNAME]: connect to the PostgreSQL database
+#
+# Example:
+#     source .../pgdb.sh "database" "user" [FUNCNAME]
+#
 # See README.md for more info
 
 
-### TEMPLATES ###
-
-# generate function definition
-# to access a database
-# ex.
-# source <(template-db "mydb" "myuser" "myfunc")
-# * myfunc [OPTION] [queryfile]
-function template-db {
-
-if [ "$1" = "-h" -o "$1" = "--help" ]
+if [ -z "$1" -o -z "$2" ]
 then
- echo "Usage: template-db [db] [user] [FUNCNAME]
-Output code for a function named [FUNCNAME] (default [db])
-to access [db] as [user].
-Option		GNU long option		Meaning
--h		--help			Show this message"
+ echo "Usage: source pgdb.sh database user [FUNCNAME]
+Output code for a function [FUNCNAME] (default [database])
+to access the PostgreSQL database [database] as [user]."
  return 0
 fi
 
-local MYDB=$1
-local MYUSER=$2
-if [ "$3" ]
-then
- local MYFUNC=$3
-else
- local MYFUNC=$MYDB
-fi
+### TEMPLATE ###
 
-cat << TEMPLATE
-# $MYDB access function
-function $MYFUNC {
+# $1 = database
+# $2 = user
+# $3 = function name
+
+eval "
+# Database access function
+function $(if [ -z "$3" ]; then printf "$1"; else printf "$3"; fi) {
 local file
 local option
-local op_time
 local state
-local good="good"
-for arg in "\$@"
+local good=\"good\"
+for arg in \"\$@\"
 do
- if [ "\$state" = "file" ]
+ if [ \"\$state\" = \"file\" ]
  then
-  if [ -e "\$arg" ]
+  if [ -e \"\$arg\" ]
   then
-   file+=( '-f' "\$arg" )
+   file+=( '-f' \"\$arg\" )
   else
-   echo "Cannot find file: \$arg"
-   good="bad"
+   echo \"Cannot find file: \$arg\"
+   good=\"bad\"
   fi
- elif [ "\$arg" = "-h" -o "\$arg" = "--help" ]
+ elif [ \"\$arg\" = \"-h\" -o \"\$arg\" = \"--help\" ]
  then
   # show help
-  echo "Usage: $MYFUNC [OPTION] [queryfile]
-Access $MYDB as $MYUSER.
+  echo \"Usage: $(if [ -z "$3" ]; then printf "$1"; else printf "$3"; fi) [OPTION] [queryfile]
+Access $1 as $2.
 If a file is given as an argument, execute the queries in the file,
-otherwise start a psql session connected to $MYDB. 
+otherwise start a psql session connected to $1. 
 Option		GNU long option		Meaning
-		--time			Time the query or session
 -h		--help			Show this message
--*		--*			Pass argument to psql (see man psql)"
+-*		--*			Pass argument to psql (see man psql)\"
   return 0
- elif [ "\$arg" = "--time" ]
+ elif [ \"\$arg\" = \"--\" ]
  then
-  op_time="true"
- elif [ "\$arg" = "--" ]
+  state=\"file\"
+ elif [[ \"\$arg\" = -* ]]
  then
-  state="file"
- elif [[ "\$arg" = -* ]]
+  option=( \"\$arg\" )
+ elif [ -e \"\$arg\" ]
  then
-  option=( "\$arg" )
- elif [ -e "\$arg" ]
- then
-  file+=( '-f' "\$arg" )
+  file+=( '-f' \"\$arg\" )
  else
-  echo "Cannot find file: \$arg"
-  good="bad"
+  echo \"Cannot find file: \$arg\"
+  good=\"bad\"
  fi
 done
 
-if [ "\$good" != "good" ]
+if [ \"\$good\" != \"good\" ]
 then
- echo "Abort"
+ echo \"Abort\"
  return 1
 fi
 
 
-if [ "\$op_time" ]
+if [ \"\${#file[@]}\" = 0 -a \"\${#option[@]}\" = 0 ]
 then
- # Time execution
- date
-fi
-
-if [ "\${#file[@]}" = 0 -a "\${#option[@]}" = 0 ]
+ psql -U \"$2\" -d \"$1\"
+elif [ \"\${#file[@]}\" = 0 ]
 then
- psql -U "$MYUSER" -d "$MYDB"
-elif [ "\${#file[@]}" = 0 ]
+ psql -U \"$2\" -d \"$1\" \"\${option[@]}\"
+elif [ \"\${#option[@]}\" = 0 ]
 then
- psql -U "$MYUSER" -d "$MYDB" "\${option[@]}"
-elif [ "\${#option[@]}" = 0 ]
-then
- psql -U "$MYUSER" -d "$MYDB" "\${file[@]}"
+ psql -U \"$2\" -d \"$1\" \"\${file[@]}\"
 else
- psql -U "$MYUSER" -d "$MYDB" "\${file[@]}" "\${option[@]}"
-fi
-
-if [ "\$op_time" ]
-then
- # Time execution
- date
+ psql -U \"$2\" -d \"$1\" \"\${file[@]}\" \"\${option[@]}\"
 fi
 }
-TEMPLATE
-} # function template-mydb
-
-
-# output function definition
-# to backup a database
-# ex.
-# source <(template-dbbackup "mydb" "mysuperuser" "mybackupdir" "myfunc")
-# * myfunc [OPTION] [file]
-function template-dbbackup {
-
-if [ "$1" = "-h" -o "$1" = "--help" ]
-then
- echo "Usage: template-dbbackup [db] [superuser] [backupdir] [FUNCNAME]
-Output code for a function named [FUNCNAME] (default [db]backup)
-to backup [db] as [superuser]. Backups placed in [backupdir] by default.
-Option		GNU long option		Meaning
--h		--help			Show this message"
- return 0
-fi
-
-local MYDB=$1
-local MYSUPERUSER=$2
-local MYBACKUPDIR=$3
-if [ "$4" ]
-then
- local MYFUNC=$4
-else
- local MYFUNC=${MYDB}backup
-fi
-
-cat << TEMPLATE
-# $MYDB backup function
-function $MYFUNC {
-if [ -z "\$1" ]
-then
- # make dated backup
- pg_dumpall -c -U "$MYSUPERUSER" > "$MYBACKUPDIR/${MYDB}_\$(date +%F).sql"
-elif [ "\$1" = "-m" ]
-then
- # move last monthly, delete previous month
- mkdir -p "$MYBACKUPDIR/monthly"
- mv "$MYBACKUPDIR/${MYDB}_\$(date -d"1 month ago" +%F).sql" "$MYBACKUPDIR/monthly" &&
- rm "$MYBACKUPDIR/${MYDB}_\$(date -d"1 month ago" +%Y-%m-)*.sql"
-elif [ "\$1" = "-h" ]
-then
- echo "Usage: $MYFUNC [OPTION] [file]
-Backup or restore $MYDB as $MYSUPERUSER.
-If no arguments given, make dated backup in $MYBACKUPDIR,
-otherwise, execute according the options below.
-Option		GNU long option		Meaning
--b		--backup		Backup $MYDB to file
--r		--restore		Restore $MYDB from file
--h		--help			Show this message"
-elif [ "\$1" = "-b" -a "\$2" ]
-then
- pg_dumpall -c -U "$MYSUPERUSER" > "\$2"
-elif [ "\$1" = "-r" -a -e "\$2" ]
-then
- psql -f "\$2" -U "$MYSUPERUSER" postgres | grep ERROR
-fi
-}
-TEMPLATE
-} # function template-dbbackup
-
+"

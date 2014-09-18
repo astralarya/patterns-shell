43573ce6aa015aef0042560a25bc441e1b197a20
Mara Kim <hacker.root@gmail.com>
2014-09-17 22:29:54 -0500

    Only update master branch

--- update.sh
+++ update.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 cd "$( dirname "${BASH_SOURCE[0]}" )" &&
+ref="$(git rev-parse --abbrev-ref HEAD)" &&
+if [ "$ref" = HEAD ]
+then ref="$(git rev-parse HEAD)"
+elif [ "$ref" = master ]
+then ref=""
+fi &&
 git pull &&
-git submodule update --recursive --init
+git submodule update --recursive --init &&
+if [ "$ref" ]
+then git checkout "$ref"
+fi

d71e7cd7969635d2b2734faf94ed30978347e76e
Mara Kim <hacker.root@gmail.com>
2014-09-30 20:46:43 -0500

    Test if merge conflict

--- update.sh
+++ update.sh
@@ -11,4 +11,5 @@ git submodule update --recursive --init &&
 if [ "$ref" ]
 then git checkout "$ref"
 git merge --strategy=recursive --strategy-option=ours master
+git merge --abort 2>/dev/null && printf 'Auto merge failed with master\n'
 fi
